1d28f3ceb0cd38c246e2dc734f37f44174f3d3d1
shelchkovy <shelchkovy@yandex.ru>
2019-12-03 20:30:11 +0400

    Added autoprefixer to sass task #10

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer');
 
 gulp.task('sass', function() {
 	return gulp.src('./app/scss/**/*.scss')
 		.pipe(sass({ includePaths: "./node_modules"}))
+		.pipe(postcss([autoprefixer]))
 		.on('error', function(error) {
 			console.log(error);
 			this.emit('end');
