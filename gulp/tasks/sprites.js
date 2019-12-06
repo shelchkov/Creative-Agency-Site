8c9c9bd2b695d6d8204f8970099ff47c399728c7
shelchkovy <shelchkovy@yandex.ru>
2019-12-06 19:33:13 +0400

    Added clean tasks #24

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-	svgSprite = require('gulp-svg-sprite');
+	svgSprite = require('gulp-svg-sprite'),
+	del = require('del');
 
 const config = {
 	mode: {
@@ -14,15 +15,23 @@ const config = {
 	}
 }
 
-gulp.task('createSprite', function() {
+gulp.task('beginClean', function() {
+	return del(['./app/temp/sprite', './app/assets/images/sprites']);
+});
+
+gulp.task('createSprite', gulp.series('beginClean', function() {
 	return gulp.src('./app/assets/images/icons/**/*.svg')
 		.pipe(svgSprite(config))
 		.pipe(gulp.dest('./app/temp/sprite'));
-});
+}));
 
 gulp.task('copySpriteGraphic', gulp.series('createSprite', function() {
 	return gulp.src('./app/temp/sprite/css/**/*.svg')
 		.pipe(gulp.dest('./app/assets/images/sprites'));
 }));
 
-gulp.task('icons', gulp.series('copySpriteGraphic'));
+gulp.task('endClean', function() {
+	return del('./app/temp/sprite');
+});
+
+gulp.task('icons', gulp.series('copySpriteGraphic', gulp.series('endClean')));
